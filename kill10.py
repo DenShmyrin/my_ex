aaaac4c69946dbdc1c5930747e40e33f2765680d
DenShmyrin <reddevil43@mail.ru>
2021-03-22 19:00:15 +0300

    Какая то из еще одних первых прог Сергея

---
+++ kill10.py
@@ -0,0 +1,11 @@
+num = input(int( "сколько будет" 2+2))
+
+sex = num
+if sex < 4:
+    print(" Ты что тупой мудак что ли?")
+elif sex > 4:
+    print(" Извилины это явно не твое!!! ")
+elif sex == 4:
+    print(" Да ты умный малый!!! ")
+else:
+    print("Something else")
